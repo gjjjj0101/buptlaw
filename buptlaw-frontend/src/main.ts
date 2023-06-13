1ade601d0d85f11cd86719f4ea7dc0892591d213
QQKdeGit <627948700@qq.com>
2023-06-13 13:58:52 +0800

    chore(frontend): import fortawesome

--- buptlaw-frontend/src/main.ts
+++ buptlaw-frontend/src/main.ts
@@ -1,9 +1,20 @@
 import { createApp } from 'vue'
+
+import { library }         from "@fortawesome/fontawesome-svg-core"
+import { FontAwesomeIcon } from '@fortawesome/vue-fontawesome'
+import { fab }             from "@fortawesome/free-brands-svg-icons"
+import { fas }             from "@fortawesome/free-solid-svg-icons"
+
+library.add(fab)
+library.add(fas)
+
 import ElementPlus from 'element-plus'
 import 'element-plus/dist/index.css'
+
 import App from './App.vue'
 
 const app = createApp(App)
 
 app.use(ElementPlus)
+app.component('font-awesome-icon', FontAwesomeIcon)
 app.mount('#app')
