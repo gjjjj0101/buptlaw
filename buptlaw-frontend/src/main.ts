d42ed13bd49efa2c4e04597244bcf137d24c3327
QQKdeGit <627948700@qq.com>
2023-06-18 13:26:11 +0800

    feat(frontend): add locale to element-plus

--- buptlaw-frontend/src/main.ts
+++ buptlaw-frontend/src/main.ts
@@ -9,15 +9,19 @@ library.add(fab)
 library.add(fas)
 
 import ElementPlus from 'element-plus'
+import zhCn        from 'element-plus/dist/locale/zh-cn.mjs'
 import 'element-plus/dist/index.css'
 
-import App from './App.vue'
+import App        from './App.vue'
 import { router } from './router'
-import { store } from './store'
+import { store }  from './store'
 
 const app = createApp(App)
 
-app.use(ElementPlus as any)
+
+app.use(ElementPlus, {
+  locale: zhCn,
+})
 app.use(router)
 app.use(store)
 app.component('font-awesome-icon', FontAwesomeIcon)
