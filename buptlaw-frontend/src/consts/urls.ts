ddd2c5516ea87bf45a7f836c90d4779d74a8e19f
QQKdeGit <627948700@qq.com>
2023-06-25 16:06:45 +0800

    feat(frontend): update url

--- buptlaw-frontend/src/consts/urls.ts
+++ buptlaw-frontend/src/consts/urls.ts
@@ -10,9 +10,8 @@
 //
 //--------------------------------------------------------------------------
 
+// export const BACKEND_URL = "http://127.0.0.1:4523/m1/2826362-0-default";
+// export const LAW_BACKEND_URL = "http://127.0.0.1:4523/m1/2826362-0-3de1c54b";
 
-export const BACKEND_URL = "http://127.0.0.1:4523/m1/2826362-0-default";
-export const LAW_BACKEND_URL = "http://127.0.0.1:4523/m1/2826362-0-3de1c54b";
-
-// export const BACKEND_URL = "http://10.28.145.233:8080"
-// export const LAW_BACKEND_URL = "http://10.28.145.233:8081"
+export const BACKEND_URL = "http://localhost:8080"
+export const LAW_BACKEND_URL = "http://localhost:8081"
