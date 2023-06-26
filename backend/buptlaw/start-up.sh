444bb71f4300b1946952a08cd3a8eee5f0831d1f
Gao Jian <1209640759@qq.com>
2023-06-26 09:02:49 +0800

    build: update start-up.sh

--- backend/buptlaw/start-up.sh
+++ backend/buptlaw/start-up.sh
@@ -4,5 +4,5 @@ java --version >/dev/null 2>&1
 if [ $? -ne 0 ]; then
     exit 1
 fi
-java -jar law-users-1.0-SNAPSHOT.jar
-java -jar law-regulations-1.0-SNAPSHOT.jar
+nohup java -jar ./law-users/target/law-users-1.0-SNAPSHOT.jar > users_out.log 2>&1 &
+nohup java -jar ./law-regulations/target/law-regulations-1.0-SNAPSHOT.jar > regulations_out.log 2>&1 &
